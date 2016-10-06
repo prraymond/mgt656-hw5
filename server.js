b24f0c7a5eb723685d11827b6ff084a1b57a5262
Paul Raymond <paulrenaud.raymond@yale.edu>
2016-10-06 22:04:13 +0000

    add nickname for Paul

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+    response.send('crowded-pheasant');
+});
+
+app.get('/crowded-pheasant', function(request, response){
+    response.send('crowded-pheasant');
 });
 
 app.listen(process.env.PORT || 4000);
